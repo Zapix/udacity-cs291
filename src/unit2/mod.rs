b939d51872703b2b8f3ee9e327b60a0b4cd89219
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2024-01-07 15:23:30 +0400

    Add polygon creation lesson

--- src/unit2/mod.rs
+++ src/unit2/mod.rs
@@ -1,14 +1,17 @@
 mod triangle_mesh;
 mod vertex_order;
+mod polygon_creation;
 
 use std::rc::Rc;
 use triangle_mesh::TriangleMesh;
 use vertex_order::VertexOrder;
+use polygon_creation::PolygonCreation;
 use crate::common::unit_trait::UnitTrait;
 
 pub fn get_units() -> Vec<Rc<Box<dyn UnitTrait>>> {
     vec![
         Rc::new(Box::new(TriangleMesh::new())),
         Rc::new(Box::new(VertexOrder::new())),
+        Rc::new(Box::new(PolygonCreation::new())),
     ]
 }
